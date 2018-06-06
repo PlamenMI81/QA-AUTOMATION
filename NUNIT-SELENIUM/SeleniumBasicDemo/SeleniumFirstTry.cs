8ee56e11edf1319818061acbbacec1aa6a859a4c
PlamenMIvanov <plamko@abv.bg>
2018-06-07 00:26:28 +0300

    NUNIT-SELENIUM

---
+++ NUNIT-SELENIUM/SeleniumBasicDemo/SeleniumFirstTry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SeleniumBasicDemo
+{
+    internal class SeleniumFirstTry
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hey");
+            BankAcount acount = new BankAcount(1m);
+            acount.Deposit(100m);
+            acount.Withdraw(500m);
+        }
+    }
+}
